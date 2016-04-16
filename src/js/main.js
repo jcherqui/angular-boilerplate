e067fd1fd4355cb6f17c5d0403b0017077977cd3
ston3o <5039608+ston3o@users.noreply.github.com>
2016-04-17 00:35:51 +0200

    Add angular-ui-router

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-import hello from './hello.js';
-
 import 'expose?$!expose?jQuery!jquery';
 import 'bootstrap';
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 
-hello();
+const app = angular.module('app', [uiRouter]);
+app.config(($stateProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            template: require('../partials/home.html'),
+        })
+    ;
+});
